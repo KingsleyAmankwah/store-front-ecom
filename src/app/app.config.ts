d380937e8693491a782cb321e0e51204ee84eee2
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-25 17:19:30 -0800

    config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ShopComponent } from './pages/shop/shop.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), ShopComponent],
 };
