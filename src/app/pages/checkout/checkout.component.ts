675c5c333d021e914e513d22d07120229eac3561
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-29 16:22:04 -0800

    checkout component designs

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -12,10 +12,23 @@ import { CommonModule } from '@angular/common';
 })
 export class CheckoutComponent {
   activeStep = 1;
+  activePaymentButton: string = 'visaCard';
 
   changeStep() {
     if (this.activeStep < 3) {
       this.activeStep++;
     }
+  }
+  showDetails(button: string): void {
+    this.activePaymentButton = button;
+  }
+
+  showOptions = false;
+  selectedOption!: string;
+
+  selectOption(option: string, event: Event) {
+    event.stopPropagation();
+    this.selectedOption = option;
+    this.showOptions = false;
   }
 }
