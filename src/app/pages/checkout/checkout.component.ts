a68cc450fcf12fccd4984508208a20fb3a7d35d1
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-01-30 17:54:32 -0800

    checkout component class

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -11,9 +11,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './checkout.component.css',
 })
 export class CheckoutComponent {
+  totalCartPrice = 0;
+  count = 1;
   activeStep = 1;
   activePaymentButton: string = 'visaCard';
+  cartItems: Item[] = [];
 
+  cartService = inject(CartService);
   changeStep() {
     if (this.activeStep < 3) {
       this.activeStep++;
@@ -30,5 +34,28 @@ export class CheckoutComponent {
     event.stopPropagation();
     this.selectedOption = option;
     this.showOptions = false;
+  }
+
+  ngOnInit() {
+    this.cartService.cartItems$.subscribe((items) => {
+      this.cartItems = items;
+      this.calculateTotalPrice();
+    });
+  }
+
+  removeFromCart(item: Item) {
+    this.cartService.removeFromCart(item);
+  }
+
+  calculateTotalPrice() {
+    // Reset totalCartPrice before recalculating
+    this.totalCartPrice = 0;
+
+    // Sum up the prices of all items in the cart
+    for (const item of this.cartItems) {
+      // Parse the price as a float (assuming it's a string in the format 'GH₵ xxx.xx')
+      const price = parseFloat(item.price.replace('GH₵', '').trim());
+      this.totalCartPrice += price * this.count;
+    }
   }
 }
