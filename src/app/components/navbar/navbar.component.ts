9078b01637f28592722c5a23e486a595f6855533
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-01 09:16:32 -0800

    auth functionality added to navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   cartItemCount = 0;
+  showLogout = false;
 
   cartService = inject(CartService);
   router = inject(Router);
+  authService = inject(AuthService);
 
   ngOnInit() {
     this.cartService.cartItems$.subscribe((items) => {
@@ -23,5 +27,22 @@ export class NavbarComponent {
 
   navigateToCart() {
     this.router.navigate(['/cart']);
+  }
+
+  login() {
+    this.authService.setLoggedIn(true);
+  }
+
+  signup() {
+    this.authService.setLoggedIn(true);
+  }
+
+  toggleLogout() {
+    this.showLogout = !this.showLogout;
+  }
+
+  logout() {
+    this.authService.logout();
+    this.showLogout = false;
   }
 }
