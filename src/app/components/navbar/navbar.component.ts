ed4d292df6df060b2bc92a6091057b9268e90735
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-02 15:00:59 +0000

    :feat/ navbar refactored

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -13,11 +12,9 @@ import { CommonModule } from '@angular/common';
 })
 export class NavbarComponent {
   cartItemCount = 0;
-  showLogout = false;
 
   cartService = inject(CartService);
   router = inject(Router);
-  authService = inject(AuthService);
 
   ngOnInit() {
     this.cartService.cartItems$.subscribe((items) => {
@@ -27,22 +24,5 @@ export class NavbarComponent {
 
   navigateToCart() {
     this.router.navigate(['/cart']);
-  }
-
-  login() {
-    this.authService.setLoggedIn(true);
-  }
-
-  signup() {
-    this.authService.setLoggedIn(true);
-  }
-
-  toggleLogout() {
-    this.showLogout = !this.showLogout;
-  }
-
-  logout() {
-    this.authService.logout();
-    this.showLogout = false;
   }
 }
