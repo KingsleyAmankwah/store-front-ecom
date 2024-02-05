63483e7f99e14f35bcc2832c264380ab8d05e9a5
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-05 11:04:52 +0000

    :feat/ getter and setter methods modified and code refactoring done

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,29 +7,33 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class CartService {
   private cartItemsSubject = new BehaviorSubject<Item[]>([]);
-  cartItems$ = this.cartItemsSubject.asObservable();
+  cartItems = this.cartItemsSubject.asObservable();
 
-  private _selectedSize: string | null = null;
+  private _selectedSize!: string;
 
-  public get selectedSize(): string | null {
+  public get selectedSize() {
     return this._selectedSize;
   }
 
-  public set selectedSize(value: string | null) {
+  public set selectedSize(value: string) {
     this._selectedSize = value;
   }
 
-  addToCart(item: Item) {
-    const currentCartItems = this.cartItemsSubject.value;
-    const itemExists = currentCartItems.some(
+  addToCart(item: Item, quantity: number = 1) {
+    const currentCartItems = this.cartItemsSubject.value || [];
+    const existingItemIndex = currentCartItems.findIndex(
       (cartItem) => cartItem.id === item.id
     );
 
-    if (!itemExists) {
-      this.cartItemsSubject.next([...currentCartItems, item]);
+    if (existingItemIndex !== -1) {
+      currentCartItems[existingItemIndex].count! += quantity;
     } else {
-      console.log(`${item.title} is already in the cart.`);
+      // Item does not exist, add it to the cart with the quantity
+      const newItem = { ...item, count: quantity };
+      currentCartItems.push(newItem);
     }
+    // Emit the updated cartItems array
+    this.cartItemsSubject.next([...currentCartItems]);
   }
 
   removeFromCart(item: Item) {
