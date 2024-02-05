5e44693830ab13153d79e22b2b40172add447c81
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-05 11:14:47 +0000

    :feat/ medthod to navigate the page to top of the screen removed

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -19,10 +19,6 @@ export class ShopComponent {
   router = inject(Router);
   cartService = inject(CartService);
 
-  ngAfterViewChecked() {
-    window.scrollTo(0, 0);
-  }
-
   goToDetails(item: Item) {
     this.router.navigate(['/details', item.id]);
   }
